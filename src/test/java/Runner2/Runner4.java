e84c56f9b9f5dbc7f6a511b7038a3866011c9b51
Ankitha991 <ankithareddy691 @gmail.com>
2024-04-03 09:15:17 +0530

    added another file

--- src/test/java/Runner2/Runner4.java
+++ src/test/java/Runner2/Runner4.java
@@ -1,19 +1,14 @@
 package Runner2;
 
-//import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
-//import io.cucumber.junit.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-//@RunWith(Cucumber.class)
 
-@CucumberOptions(features="Features",glue="StepDefinitions",plugin={"pretty","junit:target/cucumberReports.xml"})
-//plugin="com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:")
+@CucumberOptions(features="Features",glue="StepDefinitions",plugin={"pretty","junit:target/cucumberReports.xml",
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+		"json:target/cucumberReports.json"})
 
-//plugin={"pretty","json:target/cucumberReports.json"}
-
-//plugin={"pretty","junit:target/cucumberReports.xml"}
 public class Runner4 extends AbstractTestNGCucumberTests{
 
 }
